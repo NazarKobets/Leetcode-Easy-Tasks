2922ef740e9979a0c8da4b56cf6e54fea48bd128
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2023-08-23 19:28:02 +0300

    Added task description

--- ArrayPrototypeLast.js
+++ ArrayPrototypeLast.js
@@ -1,3 +1,6 @@
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element.
+// If there are no elements in the array, it should return -1.
+
 Array.prototype.last = function() {
   return this.length ? this.slice(-1)[0] : -1;
 };
--- GenerateFibonacciSequence.js
+++ GenerateFibonacciSequence.js
@@ -1,3 +1,7 @@
+// Write a generator function that returns a generator object which yields the fibonacci sequence.
+// The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+// The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+
 var fibGenerator = function*() {
   for (let a = 0, b = 1; true; b += a, a = b - a) yield a;
 };
